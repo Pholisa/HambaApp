ac67f730308f1b510972e6c7d98e73d0584fd582
Pholisa <nentlenofemele@outlook.com>
2023-10-20 13:18:31 +0200

    Changes

---
+++ app/src/main/java/com/example/hambaapp/AccomodationPage.kt
@@ -0,0 +1,11 @@
+package com.example.hambaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccomodationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accomodation_page)
+    }
+}
